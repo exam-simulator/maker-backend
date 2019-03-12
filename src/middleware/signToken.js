56b6e2a21e52151f6f1994880d1a252e088cdb39
benjaminadk <benjaminadk@gmail.com>
2019-03-12 02:04:52 -0700

    :bug: Fix cookie domain option

--- src/middleware/signToken.js
+++ src/middleware/signToken.js
@@ -1,11 +1,13 @@
 const jwt = require('jsonwebtoken')
 
-const { COOKIE, JWT_SECRET } = process.env
+const { COOKIE, JWT_SECRET, NODE_ENV } = process.env
 
 module.exports = (res, userId) => {
   const token = jwt.sign({ userId }, JWT_SECRET)
-  res.cookie(COOKIE, token, {
+  const cookieOptions = {
     httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 * 365
-  })
+    maxAge: 1000 * 60 * 60 * 24 * 365,
+    domain: NODE_ENV === 'development' ? 'localhost' : '.exam-maker.herokuapp.com'
+  }
+  res.cookie(COOKIE, token, cookieOptions)
 }
