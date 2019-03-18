93975c7a0522f479a497b3a3769a3d717274a68b
benjaminadk <benjaminadk@gmail.com>
2019-03-17 21:20:00 -0700

    :sparkles: Add email validation to signin mutation

--- src/resolvers/utils/validateSignup.js
+++ src/resolvers/utils/validateSignup.js
@@ -1,6 +1,10 @@
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
 module.exports = args => {
-  if (!args.email) throw new Error('Email is required.')
   if (!args.name) throw new Error('Name is required.')
+  if (args.name.length < 3) throw new Error('Name must be 4 characters.')
+  if (!args.email) throw new Error('Email is required.')
+  if (!emailRegex.test(args.email)) throw new Error('Email is not valid.')
   if (!args.password) throw new Error('Password is required.')
   if (args.password.length < 8) throw new Error('Password must be 8 characters.')
 }
